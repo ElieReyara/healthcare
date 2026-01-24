66abec0edeb5f62a70f5be2864714abacd0421f7
Elie REYARA <reyaraelie0@gmail.com>
2026-01-24 17:09:32 +0000

    Setup du projet

---
+++ gestion-poste-sante/src/main/java/com/healthcenter/App.java
@@ -0,0 +1,10 @@
+package com.healthcenter;
+
+/**
+ * Application principale
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Bienvenue dans le système de gestion de poste de santé!");
+    }
+}
